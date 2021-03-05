184dfff5466502412b822fd0b21bc6b875e8ff95
Richard Howard <richard@promptworks.com>
2021-03-05 15:16:09 -0500

    Better assertions

--- tests/test_pydactory.py
+++ tests/test_pydactory.py
@@ -1,10 +1,16 @@
 from tests.support import BookFactory
+from decimal import Decimal
+from hamcrest import assert_that, has_properties  # type:ignore
 
 
 def test_defaults():
     book = BookFactory.build()
-    assert book.title == "War and Peace"
-    assert len(book.reviews) == 1
+    assert_that(book, has_properties(title="War and Peace", author="Leo Tolstoy"))
+
+
+def test_casted_attr():
+    book = BookFactory.build()
+    assert book.dimensions == (Decimal("1.0"), Decimal("2.0"), Decimal("3.0"))
 
 
 def test_overrides():
