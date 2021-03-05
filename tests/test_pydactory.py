dfaf2a7b2b9a8279864746cb4000c7aad0ae258e
Richard Howard <richard@promptworks.com>
2021-03-05 14:54:24 -0500

    Test overrides

--- tests/test_pydactory.py
+++ tests/test_pydactory.py
@@ -5,3 +5,8 @@ def test_defaults():
     book = BookFactory.build()
     assert book.title == "War and Peace"
     assert len(book.reviews) == 1
+
+
+def test_overrides():
+    book = BookFactory.build(title="Warren Peas")
+    assert book.title == "Warren Peas"
