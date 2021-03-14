c3712cebf43701be02e980e5e0b05e7fe8719e52
Richard Howard <richard@promptworks.com>
2021-03-14 16:37:34 -0400

    Rename Square to Rect

--- tests/test_pydactory.py
+++ tests/test_pydactory.py
@@ -6,59 +6,59 @@ from hamcrest import assert_that, has_properties  # type:ignore
 
 
 def test_build_simple_factory():
-    class Square(BaseModel):
+    class Rect(BaseModel):
         height: int
         width: int
 
-    class SquareFactory(Factory[Square]):
+    class RectFactory(Factory[Rect]):
         height = 3
         width = 4
 
-    square = SquareFactory.build()
-    assert_that(square, has_properties(height=3, width=4))
+    rect = RectFactory.build()
+    assert_that(rect, has_properties(height=3, width=4))
 
 
 def test_build_overrides():
-    class Square(BaseModel):
+    class Rect(BaseModel):
         height: int
         width: int
 
-    class SquareFactory(Factory[Square]):
+    class RectFactory(Factory[Rect]):
         height = 3
         width = 4
 
-    square = SquareFactory.build(height=2)
-    assert_that(square, has_properties(height=2, width=4))
+    rect = RectFactory.build(height=2)
+    assert_that(rect, has_properties(height=2, width=4))
 
 
 def test_build_model_with_aliases():
-    class Square(BaseModel):
+    class Rect(BaseModel):
         height: int = Field(alias="Height")
         width: int = Field(alias="Width")
 
 
-    class SquareFactory(Factory[Square]):
+    class RectFactory(Factory[Rect]):
         height = 3
         width = 4
 
 
-    square = SquareFactory.build()
-    assert_that(square, has_properties(height=3, width=4))
+    rect = RectFactory.build()
+    assert_that(rect, has_properties(height=3, width=4))
 
 
 def test_build_model_with_aliases_and_override():
-    class Square(BaseModel):
+    class Rect(BaseModel):
         height: int = Field(alias="Height")
         width: int = Field(alias="Width")
 
 
-    class SquareFactory(Factory[Square]):
+    class RectFactory(Factory[Rect]):
         height = 3
         width = 4
 
 
-    square = SquareFactory.build(height=2)
-    assert_that(square, has_properties(height=2, width=4))
+    rect = RectFactory.build(height=2)
+    assert_that(rect, has_properties(height=2, width=4))
 
 
 def test_factory_mixins():
@@ -80,32 +80,32 @@ def test_factory_mixins():
 
 
 def test_params():
-    class Square(BaseModel):
+    class Rect(BaseModel):
         height: int
         width: int
 
 
-    class SquareFactory(Factory[Square]):
+    class RectFactory(Factory[Rect]):
         height = 3
         width = 4
 
 
-    params = SquareFactory.params(alias=False)
+    params = RectFactory.params(alias=False)
     assert params["height"] == 3
     assert params["width"] == 4
 
 
 def test_params_alias():
-    class Square(BaseModel):
+    class Rect(BaseModel):
         height: int = Field(alias="Height")
         width: int = Field(alias="Width")
 
 
-    class SquareFactory(Factory[Square]):
+    class RectFactory(Factory[Rect]):
         height = 3
         width = 4
 
 
-    params = SquareFactory.params(alias=True)
+    params = RectFactory.params(alias=True)
     assert params["Height"] == 3
     assert params["Width"] == 4
