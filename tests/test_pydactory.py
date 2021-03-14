a7b5c65f054287ca43e8654b28caa65b2c0ae234
Richard Howard <richard@promptworks.com>
2021-03-14 13:18:08 -0400

    Remove old tests

--- tests/test_pydactory.py
+++ tests/test_pydactory.py
@@ -2,56 +2,5 @@ import pytest
 
 from pydantic.main import BaseModel
 from pydactory import build_default, PydactoryError
-from tests.support import BookFactory, Language, AuthorFactory, Review, Address
-from decimal import Decimal
 from hamcrest import assert_that, has_properties  # type:ignore
 
-
-def test_defaults():
-    book = BookFactory.build()
-    assert_that(book, has_properties(title="War and Peace", language=Language.RUSSIAN))
-
-
-def test_casted_attr():
-    book = BookFactory.build()
-    assert book.dimensions == (Decimal("1.0"), Decimal("2.0"), Decimal("3.0"))
-
-
-def test_overrides():
-    book = BookFactory.build(title="Warren Peas")
-    assert book.title == "Warren Peas"
-
-
-def test_child_model():
-    book = BookFactory.build()
-    assert book.author.name
-
-
-def test_factories():
-    author = AuthorFactory.build()
-    assert isinstance(author.name, str)
-    assert author.likes_cake
-    assert author.likes_chocolate_cake is None
-    assert isinstance(author.address, Address)
-    assert author.preferred_language == Language.ENGLISH
-    assert author.dob
-
-
-def test_build_default():
-    rating = build_default(Review, comment="really liked it")
-
-    assert rating.comment == "really liked it"
-
-
-def test_cant_build_arbitrary_types():
-    class Foo:
-        ...
-
-    class BarModel(BaseModel):
-        foo: Foo
-
-        class Config:
-            arbitrary_types_allowed = True
-
-    with pytest.raises(PydactoryError):
-        build_default(BarModel)
