c8494b2cddceb598b63f38880a36d2de347ce935
Richard Howard <richard@promptworks.com>
2021-07-12 10:43:31 -0400

    Bump to 0.2.0

--- pydactory/__init__.py
+++ pydactory/__init__.py
@@ -1,6 +1,6 @@
 from .errors import PydactoryError
 from .pydactory import Factory, build_model, build_model_batch
 
-__version__ = "0.1.6"
+__version__ = "0.2.0"
 
 __all__ = ["Factory", "PydactoryError", "build_model", "build_model_batch"]
